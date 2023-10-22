c9d65befa1ecc2158d521fb83cd3d1a6abbde615
apervmm <apervm@gmail.com>
2023-10-22 13:58:56 -0700

    fcm

---
+++ weather/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import string
+import json
+import urllib.request
+
+
+def index(request):
+    if request.method == 'POST':
+        try:
+            city = request.POST['city']
+            city_url = city.replace(' ', '+')
+            apiKey = 'e0e17eb3df6ed8886f523f12d9437849'
+            res = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q="+city_url+"&appid="+apiKey).read()
+            json_data = json.loads(res)
+            data = {
+                "country_code": str(json_data['sys']['country']),
+                "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+                "temp": str(json_data['main']['temp'])+'K',
+                'pressure': str(json_data['main']['pressure']),
+                'humidity':str(json_data['main']['humidity']),
+            }
+        except Exception:
+            messages.info(request, "Invalid City Name")
+            return redirect('/')
+    else:
+        city = ""
+        data = {}
+
+    return render(request, 'index.html', {"city": city, 'data':data})
