d1f1cd960cc767bccc9034b714984a88b58e91d4
apervmm <apervm@gmail.com>
2023-10-22 14:34:44 -0700

    env

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
